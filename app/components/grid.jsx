9b0bea92da37d3c7371e3193151c358bcaadc8e4
Steve Godin <steve.god@gmail.com>
2016-04-25 11:20:00 -0700

    Map grid to squares

--- app/components/grid.jsx
+++ app/components/grid.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import _ from 'lodash';
 
+import Square from './square.jsx';
+
 export default function Grid (props) {
   const {grid} = props;
 
   return (
     <section className="grid">
       {
-        _.map(grid, (square, index) =>
-              <div key={index} className="square">{square}</div>)
+        //TODO: Create function or component generation here
+        _.map(
+          _.chunk(grid, 3),
+          squares =>
+          <section className='row'>
+            {
+              _.map(
+                squares,
+                (square, index) => <Square key={index} square={square} />)
+            }
+          </section>
+          )
       }
     </section>
   );
