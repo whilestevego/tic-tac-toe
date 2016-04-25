c5d2b2338368212f07c556461b9773298b41e4d7
Steve Godin <steve.god@gmail.com>
2016-04-25 15:41:02 -0700

    Add game tic tac toe validation logic

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import Grid from './grid.jsx';
+import _ from 'lodash';
+
+const WIN_CONDITIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+function selectIndexes(coll, indexes) {
+  return _.filter(coll, (val, index) => _.includes(indexes, index));
+}
+
+function checkRow(row) {
+  return /xxx|ooo/.test(row.join(''));
+}
+
+function validateWin(grid) {
+  return _.find(WIN_CONDITIONS, indexes => checkRow(selectIndexes(grid, indexes)));
+}
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      winningRow: [],
       activePlayer: 'x',
       grid: [
         ' ', ' ', ' ',
@@ -19,21 +44,9 @@ export default class App extends React.Component {
 
     this.makeMove = this.makeMove.bind(this);
     this.togglePlayer = this.togglePlayer.bind(this);
-    this.setMove = this.setMove.bind(this);
-  }
-
-  setMove(index) {
-    this.setState(
-      prevState => {
-        const {grid, activePlayer} = prevState;
-        grid[index] = activePlayer;
-        return {grid};
-      }
-    );
   }
 
   togglePlayer() {
-    const {activePlayer} = this.state;
     const toggle = player => ({x: 'o', o: 'x'})[player];
 
     this.setState(
@@ -42,7 +55,21 @@ export default class App extends React.Component {
   }
 
   makeMove(index) {
-    this.setMove(index);
+    const {grid, activePlayer} = this.state;
+
+    // Prevent any other moves if winning row is found
+    if (!_.isEmpty(this.state.winningRow)) return;
+
+    // Construct next turns grid
+    const newGrid = grid;
+    newGrid[index] = activePlayer;
+
+    // Check for a win
+    const winningRow = validateWin(newGrid);
+    if (winningRow) this.setState({winningRow});
+
+    // Update grid
+    this.setState({grid: newGrid});
     this.togglePlayer();
   }
 
